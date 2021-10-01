30ff4e2b4d88401a0e7e48320e5423de37a509b5
kjsang <kjsangec@gmail.com>
2021-10-01 20:41:37 +0800

    0841

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -6,3 +6,17 @@ let elNamePengguna = document.getElementById("namePengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
 })
+
+function checkPassword(){
+    let elkataLaluan = document.getElementById("kataLaluan")
+    let pass_val = elkataLaluan.value
+
+    if(pass_val.length > 5){
+        alert("Password check...OK!")
+    } else {
+        alert("Password too short!")
+    }
+}
+
+let elKL = document.getElementsByid("kataLaluan")
+elKL.onblue = checkPassword
