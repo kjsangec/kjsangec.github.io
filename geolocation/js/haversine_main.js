bc8646cb0326eed3ede45e322ee75a0e159b790c
kjsang <kjsangec@gmail.com>
2021-10-04 20:17:15 +0800

    0817

---
+++ geolocation/js/haversine_main.js
@@ -0,0 +1,34 @@
+function calculateDistance(lat, long){
+    let depulze = haversine(lat,lon,2.922561,101.650965)
+    let pyramid = haversine(lat,lon,3.073065,101.607787)
+    let klcc = haversine(lat,lon,3.158761,101.714524)
+
+    return [depulze,pyramid,klcc]
+}
+
+let elLocate = document.getElementById("locate")
+elLocate.addEventListener("click", function(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(postion){
+            let elLat = document.getElementById("lat")
+            let elLong = document.getElementById("long")
+            let elDpluze = document.getElementById("depluze")
+            let elKlcc = document.getElementById("klcc")
+            let elSunway = document.getElementById("sunway")
+
+            let userLat = postion.coords.latitude
+            let userLong = postion.coords.longitude
+
+            let distance = calculateDistance(userLat, userLong)
+
+            elLat.innerHTML = "Your latitude: " + userLat
+            elLong.innerHTML = "Your longtitude: " + userLong
+            elDpluze.innerHTML = "Distance to Dpluze, Cyberjaya is " + distance[0] + "km"
+            elKlcc.innerHTML = "Distance to KLCC is " + distance[2] + "km"
+            elSunway.innerHTML = "Distance to Sunway Pyramid is " + distance[1] + "km" 
+        })
+
+    } else {
+        alert("Geolocation is not supported!")
+    }
+})
